6fe3f2ed7b4cacfa868f145475951958ed2da9f7
Balaji <thiagarajan.balaji@gmail.com>
2021-06-03 00:11:28 +0300

    Initial commit

---
+++ apache-camel-one/src/main/java/com/bala/microservices/apachecamelone/ApacheCamelOneApplication.java
@@ -0,0 +1,13 @@
+package com.bala.microservices.apachecamelone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelOneApplication.class, args);
+	}
+
+}
---
+++ apache-camel-two/src/test/java/com/bala/microservices/apachecameltwo/ApacheCamelTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bala.microservices.apachecameltwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
